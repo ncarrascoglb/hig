ed2878e012536ebc28dd586399d751a0024f77b7
Ari Russo <ari.russo@autodesk.com>
2018-09-13 11:43:51 -0400

    feat: Adds openOnHover prop to Tooltip. This enables the Tooltip to display when a user hovers over the target

--- packages/tooltip/src/Tooltip.test.js
+++ packages/tooltip/src/Tooltip.test.js
@@ -1,12 +1,34 @@
-import { takeSnapshotsOf } from "@hig/jest-preset/helpers";
-
+import { anchorPoints } from "@hig/flyout/src";
+import Button from "@hig/button";
+import React from "react";
+import renderer from "react-test-renderer";
 import Tooltip from "./Tooltip";
 
 describe("tooltip/Tooltip", () => {
-  takeSnapshotsOf(Tooltip, [
+  [
     {
-      desc: "renders",
+      description: "renders without props",
       props: {}
+    },
+    {
+      description: "renders with props",
+      props: {
+        anchorPoint: anchorPoints.TOP_CENTER,
+        children: <Button title="Click Me" />,
+        content: "Hello World",
+        maxHeight: 150,
+        onClickOutside: function onClickOutside() {},
+        onScroll: function onScroll() {},
+        open: true,
+        openOnHover: false
+      }
     }
-  ]);
+  ].forEach(({ description, props: { children, ...otherProps } }) => {
+    it(description, () => {
+      const presenter = <Tooltip {...otherProps}>{children}</Tooltip>;
+      const tree = renderer.create(presenter).toJSON();
+
+      expect(tree).toMatchSnapshot();
+    });
+  });
 });
